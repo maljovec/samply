bc6311f4c225c2a1cb74f63d83829d229b2e0e09
Dan Maljovec <maljovec002@gmail.com>
2019-02-14 00:47:47 -0700

    Adding function to read the long_description from the README.rst file.

--- setup.py
+++ setup.py
@@ -26,13 +26,30 @@ def get_property(prop, project):
 
 VERSION = get_property("__version__", "samply")
 
+def long_description():
+    """ Reads the README.rst file and extracts the portion tagged between
+        specific LONG_DESCRIPTION comment lines.
+    """
+    description = ""
+    recording = False
+    with open("README.rst") as f:
+        for line in f:
+            if "END_LONG_DESCRIPTION" in line:
+                return description
+            elif "LONG_DESCRIPTION" in line:
+                recording = True
+                continue
+
+            if recording:
+                description += line
+
 # Consult here: https://packaging.python.org/tutorials/distributing-packages/
 setup(
     name="samply",
     packages=["samply"],
     version=VERSION,
     description="A library for computing samplings in arbitrary dimensions",
-    long_description="TODO",
+    long_description=long_description(),
     author="Dan Maljovec",
     author_email="maljovec002@gmail.com",
     license="BSD",
